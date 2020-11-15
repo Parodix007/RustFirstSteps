eb0add96240b1dcd95bbcb26a68ab28a19cd78e1
Parodix007 <sebastian.siarczynski@gmail.com>
2020-11-16 00:45:14 +0100

    Folder do tworzenia modulow

---
+++ modules/target/debug/deps/modules-bb562e9dfdb22d90.d
@@ -0,0 +1,7 @@
+/Users/sebastiansiarczynski/Rust/RustFirstSteps/modules/target/debug/deps/modules-bb562e9dfdb22d90.rmeta: src/lib.rs
+
+/Users/sebastiansiarczynski/Rust/RustFirstSteps/modules/target/debug/deps/libmodules-bb562e9dfdb22d90.rlib: src/lib.rs
+
+/Users/sebastiansiarczynski/Rust/RustFirstSteps/modules/target/debug/deps/modules-bb562e9dfdb22d90.d: src/lib.rs
+
+src/lib.rs:
---
+++ modules/target/debug/libmodules.d
@@ -0,0 +1,1 @@
+/Users/sebastiansiarczynski/Rust/RustFirstSteps/modules/target/debug/libmodules.rlib: /Users/sebastiansiarczynski/Rust/RustFirstSteps/modules/src/lib.rs
