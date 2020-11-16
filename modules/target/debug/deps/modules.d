5335db32f426c84c8e46c835f3b66def8a3da949
Parodix007 <sebastian.siarczynski@gmail.com>
2020-11-16 17:57:29 +0100

    Praca z modulami

---
+++ modules/target/debug/deps/modules.d
@@ -0,0 +1,5 @@
+/Users/sebastiansiarczynski/Rust/RustFirstSteps/modules/target/debug/deps/modules: src/main.rs
+
+/Users/sebastiansiarczynski/Rust/RustFirstSteps/modules/target/debug/deps/modules.d: src/main.rs
+
+src/main.rs:
