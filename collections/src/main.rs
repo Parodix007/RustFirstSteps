5a53b6123d8fda62443b3a9e46da61836df4b913
Parodix007 <sebastian.siarczynski@gmail.com>
2020-11-25 01:11:52 +0100

    Playing with collections

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,16 +1,38 @@
+#[derive(Debug)]
+enum DiffrentTypes {
+	Int(u16),
+	Text(String),
+	Bool(bool)
+}
 fn main() {
     let mut vec = vec![1, 2, 3];
-    let mut outcome: Vec<u32> = Vec::new();
+    let mut outcome = Vec::new();
 
     println!("{:?}", vec);
 
-    vec.push(4);
+    //vec.push(4);
 
-    println!("{:?}", vec);
+    // println!("{:?}", vec);
 
     for item in &vec {
     	outcome.push(item * item);
     }
 
-    println!("{:?}", outcome);
+    //println!("{:?}", outcome);
+
+    let number = &vec[0];
+
+    vec.push(6);
+
+    println!("{:?}", vec.get(100));
+
+    for item in &mut vec {
+    	*item *= *item;
+    }
+
+    println!("{:?}", vec);
+
+    let difftypes = vec![DiffrentTypes::Int(10), DiffrentTypes::Bool(false), DiffrentTypes::Text(String::from("dupa"))];
+
+    println!("{:?}", difftypes[0]);
 }
